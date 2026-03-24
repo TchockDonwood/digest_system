36124d2a8123c74e864a1edbdb9744c8f4165558
Alexey Dyuryagin <airoko.alex@gmail.com>
2026-03-24 15:24:39 +0500

    Добавил эндпоинты для админа + новую ORM модель для метрик

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
+from app.api.middleware.logging import log_requests_middleware
+
 from app.api.routers.auth_router import router as auth_router
 from app.api.routers.user_router import router as user_router
+from app.api.routers.admin_router import router as admin_router
 from app.api.routers.test_router import router as test_router
 from app.api.routers.subscription_router import router as subscription_router
 from app.api.routers.digest_router import router as digest_router
@@ -13,9 +16,14 @@ from app.api.routers.channel_router import router as channel_router
 app = FastAPI() # Создаем экземпляр приложения
 
 
+# Добавляем middlewares
+app.middleware("http")(log_requests_middleware)
+
+
 # Подключаем все роутеры
 app.include_router(auth_router)
 app.include_router(user_router)
+app.include_router(admin_router)
 app.include_router(subscription_router)
 app.include_router(digest_router)
 app.include_router(favorite_digest_router)
